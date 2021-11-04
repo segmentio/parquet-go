a9f236abf790c106ca58a29532fb7ef233116989
Achille Roussel <achille@segment.com>
2021-11-03 18:48:22 -0700

    simplify RLE decoder implementation

--- encoding/rle/decoder.go
+++ encoding/rle/decoder.go
@@ -16,8 +16,7 @@ type decoder struct {
 	run  runLengthDecoder
 	bit  bitPackDecoder
 
-	width    uint32
-	bitWidth uint32
+	bitWidth uint
 }
 
 func newDecoder(r io.Reader) *decoder {
@@ -29,8 +28,7 @@ func (d *decoder) Close() error {
 }
 
 func (d *decoder) SetBitWidth(bitWidth int) {
-	d.width = uint32(bitWidth+7) / 8
-	d.bitWidth = uint32(bitWidth)
+	d.bitWidth = uint(bitWidth)
 }
 
 func (d *decoder) Reset(r io.Reader) {
@@ -46,35 +44,27 @@ func (d *decoder) ReadByte() (byte, error) {
 }
 
 func (d *decoder) DecodeBoolean(data []bool) (int, error) {
-	return d.decode(len(data), 8, func(r io.Reader, offset, length int) (int, error) {
-		return d.dec.decodeBoolean(r, data[offset:offset+length])
-	})
+	return d.decode(bits.BoolToBytes(data), 8, 1)
 }
 
 func (d *decoder) DecodeInt32(data []int32) (int, error) {
-	bitWidth := coalesceUint32(d.bitWidth, 32)
-	return d.decode(len(data), bitWidth, func(r io.Reader, offset, length int) (int, error) {
-		return d.dec.decodeInt32(r, data[offset:offset+length], uint(bitWidth))
-	})
+	return d.decode(bits.Int32ToBytes(data), 32, d.bitWidth)
 }
 
 func (d *decoder) DecodeInt64(data []int64) (int, error) {
-	bitWidth := coalesceUint32(d.bitWidth, 64)
-	return d.decode(len(data), bitWidth, func(r io.Reader, offset, length int) (int, error) {
-		return d.dec.decodeInt64(r, data[offset:offset+length], uint(bitWidth))
-	})
+	return d.decode(bits.Int64ToBytes(data), 64, d.bitWidth)
 }
 
 func (d *decoder) DecodeInt96(data [][12]byte) (int, error) {
-	bitWidth := coalesceUint32(d.bitWidth, 96)
-	return d.decode(len(data), bitWidth, func(r io.Reader, offset, length int) (int, error) {
-		return d.dec.decodeInt96(r, data[offset:offset+length], uint(bitWidth))
-	})
+	return d.decode(bits.Int96ToBytes(data), 96, d.bitWidth)
 }
 
-func (d *decoder) decode(length int, bitWidth uint32, decode func(r io.Reader, offset, length int) (int, error)) (int, error) {
-	width := coalesceUint32(d.width, bitWidth/8)
-	offset := 0
+func (d *decoder) decode(data []byte, dstWidth, srcWidth uint) (int, error) {
+	wordSize := bits.ByteCount(dstWidth)
+	decoded := 0
+	if srcWidth == 0 {
+		srcWidth = dstWidth
+	}
 
 	if !d.init {
 		_, err := io.ReadFull(&d.data, d.buf[:4])
@@ -85,14 +75,14 @@ func (d *decoder) decode(length int, bitWidth uint32, decode func(r io.Reader, o
 		d.init = true
 	}
 
-	for length > 0 {
+	for len(data) > 0 {
 		if d.dec == nil {
 			u, err := binary.ReadUvarint(d)
 			if err != nil {
 				if err != io.EOF {
 					err = fmt.Errorf("decoding RLE run length: %w", err)
 				}
-				return offset, err
+				return decoded, err
 			}
 
 			count, bitpack := uint32(u>>1), (u&1) != 0
@@ -106,65 +96,60 @@ func (d *decoder) decode(length int, bitWidth uint32, decode func(r io.Reader, o
 				if count == 0 {
 					d.run.value = [12]byte{}
 				} else {
-					_, err := io.ReadFull(&d.data, d.run.value[:width])
+					length := bits.ByteCount(srcWidth)
+					_, err := io.ReadFull(&d.data, d.run.value[:length])
 					if err != nil {
-						return offset, fmt.Errorf("decoding RLE repeated value of size %d after count=%d: %w", width, count, err)
+						return decoded, fmt.Errorf("decoding RLE repeated value of length %d after count=%d: %w", length, count, err)
 					}
 				}
 				d.dec = &d.run
 			}
 		}
 
-		n, err := decode(&d.data, offset, length)
+		n, err := d.dec.decode(&d.data, data, dstWidth, srcWidth)
+		decoded += n / wordSize
+
 		if err != nil {
 			if err == io.EOF {
 				d.dec = nil
 			} else {
-				return offset + n, fmt.Errorf("decoding RLE values: %w", err)
+				return decoded, fmt.Errorf("decoding RLE values: %w", err)
 			}
 		}
 
-		offset += n
-		length -= n
+		data = data[n:]
 	}
 
-	return offset, nil
+	return decoded, nil
 }
 
 type hybridDecoder interface {
-	decodeBoolean(io.Reader, []bool) (int, error)
-	decodeInt32(io.Reader, []int32, uint) (int, error)
-	decodeInt64(io.Reader, []int64, uint) (int, error)
-	decodeInt96(io.Reader, [][12]byte, uint) (int, error)
+	decode(io.Reader, []byte, uint, uint) (int, error)
 }
 
 // large enougth to contain 8 x int96
 const bitBufferSize = 96
 
 type bitPackDecoder struct {
+	// Number of values that remain to be deocded, this field is initialized by
+	// the decoder from reading the bit-pack header.
 	remain int
+	// Offset of in the bit-pack buffer of the first byte that contains data
+	// loaded from the io.Reader.
 	offset int
+	// Number of bytes loaded from the io.Reader; starts at the beginning of the
+	// buffer (the byte at index 0).
 	length int
+	// Buffer where bits are loaded from the io.Reader; the size is large enough
+	// to contain 8 int96 values, since bit-pack integers are encoded in chunks
+	// of 8 values. Most of the time the source bit-width is a lot smaller than
+	// 96 bits (commonly 2-3 bits), so a lot more values get loaded in each read
+	// from the io.Reader.
 	buffer [bitBufferSize]byte
 }
 
-func (d *bitPackDecoder) decodeBoolean(r io.Reader, data []bool) (int, error) {
-	return d.decode(r, bits.BoolToBytes(data), 8, 1)
-}
-
-func (d *bitPackDecoder) decodeInt32(r io.Reader, data []int32, bitWidth uint) (int, error) {
-	return d.decode(r, bits.Int32ToBytes(data), 32, bitWidth)
-}
-
-func (d *bitPackDecoder) decodeInt64(r io.Reader, data []int64, bitWidth uint) (int, error) {
-	return d.decode(r, bits.Int64ToBytes(data), 64, bitWidth)
-}
-
-func (d *bitPackDecoder) decodeInt96(r io.Reader, data [][12]byte, bitWidth uint) (int, error) {
-	return d.decode(r, bits.Int96ToBytes(data), 96, bitWidth)
-}
-
 func (d *bitPackDecoder) decode(r io.Reader, data []byte, dstWidth, srcWidth uint) (int, error) {
+	wordSize := bits.ByteCount(dstWidth)
 	remained := d.remain
 	if remained == 0 {
 		return 0, io.EOF
@@ -188,7 +173,7 @@ func (d *bitPackDecoder) decode(r io.Reader, data []byte, dstWidth, srcWidth uin
 
 			n, err := io.ReadFull(r, d.buffer[:numberOfBytes])
 			if err != nil {
-				return remained - d.remain, err
+				return (remained - d.remain) * wordSize, err
 			}
 
 			// At this point we have the guarantee that the number of bytes read
@@ -217,7 +202,7 @@ func (d *bitPackDecoder) decode(r io.Reader, data []byte, dstWidth, srcWidth uin
 		data = data[bits.ByteCount(uint(unpacked)*dstWidth):]
 
 		if d.remain == 0 || len(data) == 0 {
-			return remained - d.remain, nil
+			return (remained - d.remain) * wordSize, nil
 		}
 	}
 }
@@ -227,50 +212,21 @@ type runLengthDecoder struct {
 	value [12]byte
 }
 
-func (d *runLengthDecoder) decodeBoolean(r io.Reader, data []bool) (int, error) {
+func (d *runLengthDecoder) decode(r io.Reader, data []byte, dstWidth, srcWidth uint) (int, error) {
 	if d.count == 0 {
 		return 0, io.EOF
 	}
-	if len(data) > int(d.count) {
-		data = data[:d.count]
-	}
-	bits.Fill(bits.BoolToBytes(data), d.value[:1])
-	d.count -= uint32(len(data))
-	return len(data), nil
-}
 
-func (d *runLengthDecoder) decodeInt32(r io.Reader, data []int32, _ uint) (int, error) {
-	if d.count == 0 {
-		return 0, io.EOF
-	}
-	if len(data) > int(d.count) {
-		data = data[:d.count]
-	}
-	bits.Fill(bits.Int32ToBytes(data), d.value[:4])
-	d.count -= uint32(len(data))
-	return len(data), nil
-}
+	wordSize := bits.ByteCount(dstWidth)
+	count := len(data) / wordSize
+	data = data[:count*wordSize]
 
-func (d *runLengthDecoder) decodeInt64(r io.Reader, data []int64, _ uint) (int, error) {
-	if d.count == 0 {
-		return 0, io.EOF
+	if n := wordSize * int(d.count); n < len(data) {
+		data, count = data[:n], int(d.count)
 	}
-	if len(data) > int(d.count) {
-		data = data[:d.count]
-	}
-	bits.Fill(bits.Int64ToBytes(data), d.value[:8])
-	d.count -= uint32(len(data))
-	return len(data), nil
-}
 
-func (d *runLengthDecoder) decodeInt96(r io.Reader, data [][12]byte, _ uint) (int, error) {
-	if d.count == 0 {
-		return 0, io.EOF
-	}
-	if len(data) > int(d.count) {
-		data = data[:d.count]
-	}
-	bits.Fill(bits.Int96ToBytes(data), d.value[:])
-	d.count -= uint32(len(data))
+	bits.Fill(data, d.value[:wordSize])
+
+	d.count -= uint32(count)
 	return len(data), nil
 }
