930c6956737cc5ecbdf41ddb74b9d719e52fcf5f
Achille Roussel <achille@segment.com>
2022-01-05 00:54:31 -0800

    remove column_chunk.go

--- row_group.go
+++ row_group.go
@@ -6,6 +6,26 @@ import (
 	"io"
 )
 
+// The ColumnChunk interface represents individual columns of a row group.
+type ColumnChunk interface {
+	// Returns the index of this column in its parent row group.
+	Column() int
+
+	// Returns a reader exposing the pages of the column.
+	Pages() PageReader
+
+	// Returns the components of the page index for this column chunk,
+	// containing details about the content and location of pages within the
+	// chunk.
+	//
+	// Note that the returned value may be the same across calls to these
+	// methods, programs must treat those as read-only.
+	//
+	// If the column chunk does not have a page index, the methods return nil.
+	ColumnIndex() *ColumnIndex
+	OffsetIndex() *OffsetIndex
+}
+
 // RowGroup is an interface representing a parquet row group.
 type RowGroup interface {
 	// Returns the number of rows in the group.
