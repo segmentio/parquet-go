27415d2a3605d0a85699d17b669890a4ffec1034
Achille Roussel <achille@segment.com>
2022-01-28 16:29:42 -0800

    remove vectorized xxhash code because the gains are too low to be worth the complexity

--- bloom/xxhash/sum64uint_amd64.s
+++ bloom/xxhash/sum64uint_amd64.s
@@ -14,115 +14,6 @@
 #define prime4 R15
 #define prime5 R15 // same as prime4 because they are not used together
 
-#define prime1YMM Y12
-#define prime2YMM Y13
-#define prime3YMM Y14
-#define prime4YMM Y15
-#define prime5YMM Y15
-
-#define tmp1YMM Y6
-#define tmp2YMM Y7
-#define tmp3YMM Y8
-#define tmp4YMM Y9
-#define tmp5YMM Y10
-#define tmp6YMM Y11
-
-DATA prime1vec<>+0(SB)/8, $PRIME1
-DATA prime1vec<>+8(SB)/8, $PRIME1
-DATA prime1vec<>+16(SB)/8, $PRIME1
-DATA prime1vec<>+24(SB)/8, $PRIME1
-GLOBL prime1vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime2vec<>+0(SB)/8, $PRIME2
-DATA prime2vec<>+8(SB)/8, $PRIME2
-DATA prime2vec<>+16(SB)/8, $PRIME2
-DATA prime2vec<>+24(SB)/8, $PRIME2
-GLOBL prime2vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime3vec<>+0(SB)/8, $PRIME3
-DATA prime3vec<>+8(SB)/8, $PRIME3
-DATA prime3vec<>+16(SB)/8, $PRIME3
-DATA prime3vec<>+24(SB)/8, $PRIME3
-GLOBL prime3vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime4vec<>+0(SB)/8, $PRIME4
-DATA prime4vec<>+8(SB)/8, $PRIME4
-DATA prime4vec<>+16(SB)/8, $PRIME4
-DATA prime4vec<>+24(SB)/8, $PRIME4
-GLOBL prime4vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime5vec<>+0(SB)/8, $PRIME5
-DATA prime5vec<>+8(SB)/8, $PRIME5
-DATA prime5vec<>+16(SB)/8, $PRIME5
-DATA prime5vec<>+24(SB)/8, $PRIME5
-GLOBL prime5vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime5plus1vec<>+0(SB)/8, $PRIME5+1
-DATA prime5plus1vec<>+8(SB)/8, $PRIME5+1
-DATA prime5plus1vec<>+16(SB)/8, $PRIME5+1
-DATA prime5plus1vec<>+24(SB)/8, $PRIME5+1
-GLOBL prime5plus1vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime5plus2vec<>+0(SB)/8, $PRIME5+2
-DATA prime5plus2vec<>+8(SB)/8, $PRIME5+2
-DATA prime5plus2vec<>+16(SB)/8, $PRIME5+2
-DATA prime5plus2vec<>+24(SB)/8, $PRIME5+2
-GLOBL prime5plus2vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime5plus4vec<>+0(SB)/8, $PRIME5+4
-DATA prime5plus4vec<>+8(SB)/8, $PRIME5+4
-DATA prime5plus4vec<>+16(SB)/8, $PRIME5+4
-DATA prime5plus4vec<>+24(SB)/8, $PRIME5+4
-GLOBL prime5plus4vec<>(SB), RODATA|NOPTR, $32
-
-DATA prime5plus8vec<>+0(SB)/8, $PRIME5+8
-DATA prime5plus8vec<>+8(SB)/8, $PRIME5+8
-DATA prime5plus8vec<>+16(SB)/8, $PRIME5+8
-DATA prime5plus8vec<>+24(SB)/8, $PRIME5+8
-GLOBL prime5plus8vec<>(SB), RODATA|NOPTR, $32
-
-DATA lowbytemask<>+0(SB)/8, $0xFF
-DATA lowbytemask<>+8(SB)/8, $0xFF
-DATA lowbytemask<>+16(SB)/8, $0xFF
-DATA lowbytemask<>+24(SB)/8, $0xFF
-GLOBL lowbytemask<>(SB), RODATA|NOPTR, $32
-
-#define mulvec4x64(tmp1, tmp2, a, b, m) \
-    VPSRLQ $32, b, m \
-    VPSRLQ $32, a, tmp2 \
-    VPMULUDQ a, m, m \
-    VPMULUDQ b, tmp2, tmp2 \
-    VPMULUDQ a, b, tmp1 \
-    VPADDQ tmp2, m, m \
-    VPSLLQ $32, m, m \
-    VPADDQ m, tmp1, m
-
-#define rotvec4x64(tmp, rot, acc) \
-    VMOVDQA acc, tmp \
-    VPSRLQ $(64 - rot), tmp, tmp \
-    VPSLLQ $rot, acc, acc \
-    VPOR tmp, acc, acc
-
-#define round4x64(tmp1, tmp2, input, acc) \
-    mulvec4x64(tmp1, tmp2, prime2YMM, input, acc) \
-    VPXOR input, input, input \
-    VPADDQ input, acc, input \
-    rotvec4x64(tmp1, 31, input) \
-    mulvec4x64(tmp1, tmp2, prime1YMM, input, acc)
-
-#define avalanche4x64(tmp1, tmp2, tmp3, acc) \
-    VMOVDQA acc, tmp1 \
-    VPSRLQ $33, tmp1, tmp1 \
-    VPXOR acc, tmp1, tmp1 \
-    mulvec4x64(tmp2, tmp3, prime2YMM, tmp1, acc) \
-    VMOVDQA acc, tmp1 \
-    VPSRLQ $29, tmp1, tmp1 \
-    VPXOR acc, tmp1, tmp1 \
-    mulvec4x64(tmp2, tmp3, prime3YMM, tmp1, acc) \
-    VMOVDQA acc, tmp1 \
-    VPSRLQ $32, tmp1, tmp1 \
-    VPXOR tmp1, acc, acc
-
 #define round(input, acc) \
 	IMULQ prime2, input \
 	ADDQ  input, acc \
@@ -159,46 +50,6 @@ TEXT ·MultiSum64Uint8(SB), NOSPLIT, $0-54
     MOVQ CX, ret+48(FP)
 
     XORQ SI, SI
-    MOVQ CX, DI
-    SHRQ $3, DI
-    SHLQ $3, DI
-
-    CMPQ DI, $8
-    JB loop
-
-    VMOVDQA prime1vec<>(SB), prime1YMM
-    VMOVDQA prime2vec<>(SB), prime2YMM
-    VMOVDQA prime3vec<>(SB), prime3YMM
-    VMOVDQA prime5vec<>(SB), prime5YMM
-loop4x64:
-    VMOVDQA prime5plus1vec<>(SB), Y0
-    VMOVDQA prime5plus1vec<>(SB), Y3
-
-    VPMOVZXBQ (BX)(SI*1), Y1
-    VPMOVZXBQ 4(BX)(SI*1), Y4
-
-    mulvec4x64(tmp1YMM, tmp2YMM, prime5YMM, Y1, Y2)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime5YMM, Y4, Y5)
-
-    VPXOR Y2, Y0, Y0
-    VPXOR Y5, Y3, Y3
-
-    rotvec4x64(tmp1YMM, 11, Y0)
-    rotvec4x64(tmp1YMM, 11, Y3)
-
-    mulvec4x64(tmp1YMM, tmp2YMM, prime1YMM, Y0, Y1)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime1YMM, Y3, Y4)
-
-    avalanche4x64(tmp1YMM, tmp2YMM, tmp3YMM, Y1)
-    avalanche4x64(tmp4YMM, tmp5YMM, tmp6YMM, Y4)
-
-    VMOVDQU Y1, (AX)(SI*8)
-    VMOVDQU Y4, 32(AX)(SI*8)
-
-    ADDQ $8, SI
-    CMPQ SI, DI
-    JB loop4x64
-    VZEROUPPER
 loop:
     CMPQ SI, CX
     JE done
@@ -236,56 +87,6 @@ TEXT ·MultiSum64Uint16(SB), NOSPLIT, $0-54
     MOVQ CX, ret+48(FP)
 
     XORQ SI, SI
-    MOVQ CX, DI
-    SHRQ $3, DI
-    SHLQ $3, DI
-
-    CMPQ DI, $8
-    JB loop
-
-    VMOVDQA prime1vec<>(SB), prime1YMM
-    VMOVDQA prime2vec<>(SB), prime2YMM
-    VMOVDQA prime3vec<>(SB), prime3YMM
-    VMOVDQA prime5vec<>(SB), prime5YMM
-loop4x64:
-    VMOVDQA prime5plus2vec<>(SB), Y0
-    VMOVDQA prime5plus2vec<>(SB), Y3
-
-    VPMOVZXWQ (BX)(SI*2), Y1
-    VPMOVZXWQ 8(BX)(SI*2), Y4
-    VPSRLQ $8, Y1, tmp3YMM
-    VPSRLQ $8, Y4, tmp6YMM
-    VPAND lowbytemask<>(SB), Y1, Y1
-    VPAND lowbytemask<>(SB), Y4, Y4
-
-    mulvec4x64(tmp1YMM, tmp2YMM, prime5YMM, Y1, Y2)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime5YMM, Y4, Y5)
-    VPXOR Y2, Y0, Y0
-    VPXOR Y5, Y3, Y3
-    rotvec4x64(tmp1YMM, 11, Y0)
-    rotvec4x64(tmp1YMM, 11, Y3)
-    mulvec4x64(tmp1YMM, tmp2YMM, prime1YMM, Y0, Y1)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime1YMM, Y3, Y4)
-
-    mulvec4x64(tmp1YMM, tmp2YMM, prime5YMM, tmp3YMM, Y2)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime5YMM, tmp6YMM, Y5)
-    VPXOR Y2, Y1, Y0
-    VPXOR Y5, Y4, Y3
-    rotvec4x64(tmp1YMM, 11, Y0)
-    rotvec4x64(tmp1YMM, 11, Y3)
-    mulvec4x64(tmp1YMM, tmp2YMM, prime1YMM, Y0, Y1)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime1YMM, Y3, Y4)
-
-    avalanche4x64(tmp1YMM, tmp2YMM, tmp3YMM, Y1)
-    avalanche4x64(tmp4YMM, tmp5YMM, tmp6YMM, Y4)
-
-    VMOVDQU Y1, (AX)(SI*8)
-    VMOVDQU Y4, 32(AX)(SI*8)
-
-    ADDQ $8, SI
-    CMPQ SI, DI
-    JB loop4x64
-    VZEROUPPER
 loop:
     CMPQ SI, CX
     JE done
@@ -332,48 +133,6 @@ TEXT ·MultiSum64Uint32(SB), NOSPLIT, $0-54
     MOVQ CX, ret+48(FP)
 
     XORQ SI, SI
-    MOVQ CX, DI
-    SHRQ $3, DI
-    SHLQ $3, DI
-
-    CMPQ DI, $8
-    JB loop
-
-    VMOVDQA prime1vec<>(SB), prime1YMM
-    VMOVDQA prime2vec<>(SB), prime2YMM
-    VMOVDQA prime3vec<>(SB), prime3YMM
-loop4x64:
-    VMOVDQA prime5plus4vec<>(SB), Y0
-    VMOVDQA prime5plus4vec<>(SB), Y3
-
-    VPMOVZXDQ (BX)(SI*4), Y1
-    VPMOVZXDQ 16(BX)(SI*4), Y4
-
-    mulvec4x64(tmp1YMM, tmp2YMM, prime1YMM, Y1, Y2)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime1YMM, Y4, Y5)
-
-    VPXOR Y2, Y0, Y0
-    VPXOR Y5, Y3, Y3
-
-    rotvec4x64(tmp1YMM, 23, Y0)
-    rotvec4x64(tmp1YMM, 23, Y3)
-
-    mulvec4x64(tmp1YMM, tmp2YMM, prime2YMM, Y0, Y1)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime2YMM, Y3, Y4)
-
-    VPADDQ prime3YMM, Y1, Y1
-    VPADDQ prime3YMM, Y4, Y4
-
-    avalanche4x64(tmp1YMM, tmp2YMM, tmp3YMM, Y1)
-    avalanche4x64(tmp4YMM, tmp5YMM, tmp6YMM, Y4)
-
-    VMOVDQU Y1, (AX)(SI*8)
-    VMOVDQU Y4, 32(AX)(SI*8)
-
-    ADDQ $8, SI
-    CMPQ SI, DI
-    JB loop4x64
-    VZEROUPPER
 loop:
     CMPQ SI, CX
     JE done
@@ -412,52 +171,6 @@ TEXT ·MultiSum64Uint64(SB), NOSPLIT, $0-54
     MOVQ CX, ret+48(FP)
 
     XORQ SI, SI
-    MOVQ CX, DI
-    SHRQ $3, DI
-    SHLQ $3, DI
-
-    CMPQ DI, $8
-    JB loop
-
-    VMOVDQA prime1vec<>(SB), prime1YMM
-    VMOVDQA prime2vec<>(SB), prime2YMM
-    VMOVDQA prime3vec<>(SB), prime3YMM
-    VMOVDQA prime4vec<>(SB), prime4YMM
-loop4x64:
-    VMOVDQA prime5plus8vec<>(SB), Y0
-    VMOVDQA prime5plus8vec<>(SB), Y3
-
-    VMOVDQU (BX)(SI*8), Y1
-    VMOVDQU 32(BX)(SI*8), Y4
-
-    VPXOR Y2, Y2, Y2
-    VPXOR Y5, Y5, Y5
-
-    round4x64(tmp1YMM, tmp2YMM, Y1, Y2)
-    round4x64(tmp4YMM, tmp5YMM, Y4, Y5)
-
-    VPXOR Y2, Y0, Y0
-    VPXOR Y5, Y3, Y3
-
-    rotvec4x64(tmp1YMM, 27, Y0)
-    rotvec4x64(tmp3YMM, 27, Y3)
-
-    mulvec4x64(tmp1YMM, tmp2YMM, prime1YMM, Y0, Y1)
-    mulvec4x64(tmp4YMM, tmp5YMM, prime1YMM, Y3, Y4)
-
-    VPADDQ prime4YMM, Y1, Y1
-    VPADDQ prime4YMM, Y4, Y4
-
-    avalanche4x64(tmp1YMM, tmp2YMM, tmp3YMM, Y1)
-    avalanche4x64(tmp4YMM, tmp5YMM, tmp6YMM, Y4)
-
-    VMOVDQU Y1, (AX)(SI*8)
-    VMOVDQU Y4, 32(AX)(SI*8)
-
-    ADDQ $8, SI
-    CMPQ SI, DI
-    JB loop4x64
-    VZEROUPPER
 loop:
     CMPQ SI, CX
     JE done
@@ -496,25 +209,6 @@ TEXT ·MultiSum64Uint128(SB), NOSPLIT, $0-54
     CMOVQGT DX, CX
     MOVQ CX, ret+48(FP)
 
-    // I attempted to vectorize this algorithm but it yielded similar or
-    // worse performance:
-    //
-    // * When computing one hash per 128 bit halves of YMM registers,
-    //   throughput was half of what we get with the scalar version.
-    //
-    // * When computing two hashes per 128 bit halves of YMM register,
-    //   the performance were similar to the scalar version with an
-    //   excessive amount of added complexity.
-    //
-    // Hashing 128 bits inputs performs two iterations of the 64 bits loop,
-    // so it makes sense that the vectorized version ended up being half as
-    // fast as hashing 64 bits values, it is running 2x the number of CPU
-    // instructions, and does not offer more opportunities to parallelize
-    // computation..
-    //
-    // I also tried unrolling two iterations of the loop using scalar
-    // instructions but the throughput improvements were minimal for a lot
-    // of added complexity.
     XORQ SI, SI
 loop:
     CMPQ SI, CX
