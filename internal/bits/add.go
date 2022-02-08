ca329c3c618d88e0b3fff73a4227e964ca0ff8ad
Achille Roussel <achille@segment.com>
2022-02-08 00:58:02 +0000

    add AVX-512 vectorized add/sub functions

--- internal/bits/add.go
+++ internal/bits/add.go
@@ -1,13 +1,5 @@
 package bits
 
-func AddInt32(data []int32, value int32) {
-	for i := range data {
-		data[i] += value
-	}
-}
+func AddInt32(data []int32, value int32) { addInt32(data, value) }
 
-func AddInt64(data []int64, value int64) {
-	for i := range data {
-		data[i] += value
-	}
-}
+func AddInt64(data []int64, value int64) { addInt64(data, value) }
