4fa570c7a6091410ba46633f0bbc3e3d62b87d8f
Achille <achille@segment.com>
2022-02-10 16:56:09 -0800

    [O11Y-197] optimize add/sub algorithms on x86 with AVX-512 support (#60)
    * add tests and benchmarks for bits.Min functions
    
    * add vectorized implementations of the bits.Min functions
    
    * add assembly version of the BE128 min
    
    * add vectorized min of BE128 values
    
    * optimize byte counts
    
    * add vectorized implementation of the bits.MinBool function
    
    * improve comparisons of big-endian 128 bits values
    
    * add tests for max functions + fix bits.MaxBool on empty inputs
    
    * port min algorithms to max
    
    * add tests for min-max algorithms
    
    * add vectorized implementation of bits.MinMax functions
    
    * improve testing
    
    * better variable names
    
    * add AVX-512 vectorized add/sub functions
    
    * Update internal/bits/max_amd64.s
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * Update internal/bits/minmax_amd64.go
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * Update internal/bits/minmax_amd64.go
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * Update internal/bits/max_amd64.h
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * Update internal/bits/max_amd64.h
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * fix typos
    
    * fix AVX-512 checks
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>

--- internal/bits/bits_amd64.go
+++ internal/bits/bits_amd64.go
@@ -2,10 +2,25 @@ package bits
 
 import "golang.org/x/sys/cpu"
 
+// All functions require at least the F and VL instruction sets (Foundation and
+// Vector Length extensions). Note that Foundation is a requirement of AVX-512,
+// and usually includes Vector Length extensions as well, so we could simply be
+// testing HasAVX512 but we leave the other checks to be more explicit about our
+// intentions, and maybe be more prepared for unconventional implementations.
 var hasAVX512 = cpu.X86.HasAVX512 &&
 	cpu.X86.HasAVX512F &&
 	cpu.X86.HasAVX512VL
 
+// For min/max functions over big-endian 128 bits values, we need the follwing
+// instructions from the DQ set:
+// * VPBROADCASTQ (with 64 bits source register)
+// * VBROADCASTI64X2
+var hasAVX512MinMaxBE128 = hasAVX512 &&
+	cpu.X86.HasAVX512DQ
+
+// For min/max functions over boolean values, we need the following instructions
+// from the VPOPCNTDQ set:
+// * VPOPCNTDQ
 var hasAVX512MinMaxBool = hasAVX512 &&
 	cpu.X86.HasAVX512VPOPCNTDQ
 
--- internal/bits/sub.go
+++ internal/bits/sub.go
@@ -1,13 +1,5 @@
 package bits
 
-func SubInt32(data []int32, value int32) {
-	for i := range data {
-		data[i] -= value
-	}
-}
+func SubInt32(data []int32, value int32) { subInt32(data, value) }
 
-func SubInt64(data []int64, value int64) {
-	for i := range data {
-		data[i] -= value
-	}
-}
+func SubInt64(data []int64, value int64) { subInt64(data, value) }
