d211e7fc9ca164e8260913847ea27043d8f7603c
Frederic Branczyk <fbranczyk@gmail.com>
2022-03-10 17:10:14 +0100

    Add test reproducing unexpected fixed size binary dict error

--- writer_test.go
+++ writer_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 	"testing/quick"
 
+	"github.com/google/uuid"
 	"github.com/hexops/gotextdiff"
 	"github.com/hexops/gotextdiff/myers"
 	"github.com/hexops/gotextdiff/span"
@@ -525,5 +526,36 @@ func TestWriterGenerateBloomFilters(t *testing.T) {
 
 	if err := quick.Check(f, nil); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestWriterRepeatedUUIDDict(t *testing.T) {
+	records := []struct {
+		List []uuid.UUID `parquet:"list,dict"`
+	}{{
+		List: []uuid.UUID{
+			uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+		},
+	}}
+	schema := parquet.SchemaOf(&records[0])
+	b := bytes.NewBuffer(nil)
+	w := parquet.NewWriter(b, schema)
+	err := w.Write(records[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	f, err := parquet.OpenFile(bytes.NewReader(b.Bytes()), int64(b.Len()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = f.RowGroup(0).Rows().ReadRow(nil)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
