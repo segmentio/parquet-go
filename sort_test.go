e61480c9c05b1cc5f015ac6023fc5e6a11362114
Frederic Branczyk <fbranczyk@gmail.com>
2022-03-16 12:01:19 +0100

    Add failing test for comparing repeated columns
    This test tests a special edge case where list `a` is a subset of list
    `b`. In this case the comparisons should end when `i == len(a)` and
    `a < b` is the result.

--- sort_test.go
+++ sort_test.go
@@ -4,6 +4,7 @@ import (
 	"sort"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/segmentio/parquet-go"
 )
 
@@ -41,5 +42,41 @@ func TestSortFunc(t *testing.T) {
 			t.Errorf("value at index %d mismatch: got=%+v want=%+v\n%+v\n%+v", i, expect[i], values[i], expect, values)
 			break
 		}
+	}
+}
+
+func TestRepeatedUUIDSortFunc(t *testing.T) {
+	type testStruct struct {
+		List []uuid.UUID `parquet:"list"`
+	}
+
+	s := parquet.SchemaOf(&testStruct{})
+
+	a := s.Deconstruct(nil, &testStruct{
+		List: []uuid.UUID{
+			uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+			uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+		},
+	})
+
+	b := s.Deconstruct(nil, &testStruct{
+		List: []uuid.UUID{
+			uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+			uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+			uuid.MustParse("00000000-0000-0000-0000-000000000003"),
+		},
+	})
+
+	// a and b are equal up until the third element, then a ends so a < b.
+	f := parquet.SortFuncOf(
+		s.ChildByName("list").Type(),
+		parquet.SortDescending(false),
+		parquet.SortNullsFirst(true),
+		parquet.SortMaxDefinitionLevel(1),
+		parquet.SortMaxRepetitionLevel(1),
+	)
+	cmp := f(a, b)
+	if cmp >= 0 {
+		t.Fatal("expected a < b, got compare value", cmp)
 	}
 }
