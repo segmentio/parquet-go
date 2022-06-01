f44049071e3040cef80fe91200f863aa29f330f3
Achille <achille@segment.com>
2022-06-01 15:40:29 -0700

    add the "split" struct field tag to configure the BYTE_STREAM_SPLIT encoding (#204)

--- schema_test.go
+++ schema_test.go
@@ -57,6 +57,17 @@ func TestSchemaOf(t *testing.T) {
 
 		{
 			value: new(struct {
+				Short float32 `parquet:"short,split"`
+				Long  float64 `parquet:"long,split"`
+			}),
+			print: `message {
+	required float short;
+	required double long;
+}`,
+		},
+
+		{
+			value: new(struct {
 				Inner struct {
 					FirstName          string `parquet:"first_name"`
 					ShouldNotBePresent string `parquet:"-"`
