cf57961a4df976a8019ff97ba7f72bd0e62d9aa0
Achille Roussel <achille@segment.com>
2022-06-08 18:44:53 +0000

    remove unused functions

--- internal/bits/bits.go
+++ internal/bits/bits.go
@@ -14,22 +14,6 @@ func ByteCount(count uint) int {
 	return int((count + 7) / 8)
 }
 
-func Round(count uint) uint {
-	return BitCount(ByteCount(count))
-}
-
-func NearestPowerOfTwo(v int) int {
-	return int(NearestPowerOfTwo64(uint64(v)))
-}
-
-func NearestPowerOfTwo32(v uint32) uint32 {
-	return 1 << uint(bits.Len32(v-1))
-}
-
-func NearestPowerOfTwo64(v uint64) uint64 {
-	return 1 << uint(bits.Len64(v-1))
-}
-
 func Len8(i int8) int {
 	return bits.Len8(uint8(i))
 }
@@ -44,20 +28,4 @@ func Len32(i int32) int {
 
 func Len64(i int64) int {
 	return bits.Len64(uint64(i))
-}
-
-func IndexShift8(bitIndex uint) (index, shift uint) {
-	return bitIndex / 8, bitIndex % 8
-}
-
-func IndexShift16(bitIndex uint) (index, shift uint) {
-	return bitIndex / 16, bitIndex % 16
-}
-
-func IndexShift32(bitIndex uint) (index, shift uint) {
-	return bitIndex / 32, bitIndex % 32
-}
-
-func IndexShift64(bitIndex uint) (index, shift uint) {
-	return bitIndex / 64, bitIndex % 64
 }
--- internal/bits/bits_test.go
+++ internal/bits/bits_test.go
@@ -7,33 +7,6 @@ import (
 	"github.com/segmentio/parquet-go/internal/bits"
 )
 
-func TestNearestPowerOfTwo(t *testing.T) {
-	for _, test := range []struct {
-		input  uint32
-		output uint32
-	}{
-		{input: 0, output: 0},
-		{input: 1, output: 1},
-		{input: 2, output: 2},
-		{input: 3, output: 4},
-		{input: 4, output: 4},
-		{input: 5, output: 8},
-		{input: 6, output: 8},
-		{input: 7, output: 8},
-		{input: 8, output: 8},
-		{input: 30, output: 32},
-	} {
-		t.Run(fmt.Sprintf("NearestPowerOfTwo(%d)", test.input), func(t *testing.T) {
-			if nextPow2 := bits.NearestPowerOfTwo32(test.input); nextPow2 != test.output {
-				t.Errorf("wrong 32 bits value: want=%d got=%d", test.output, nextPow2)
-			}
-			if nextPow2 := bits.NearestPowerOfTwo64(uint64(test.input)); nextPow2 != uint64(test.output) {
-				t.Errorf("wrong 64 bits value: want=%d got=%d", test.output, nextPow2)
-			}
-		})
-	}
-}
-
 func TestBitCount(t *testing.T) {
 	for _, test := range []struct {
 		bytes int
@@ -71,26 +44,6 @@ func TestByteCount(t *testing.T) {
 		t.Run(fmt.Sprintf("ByteCount(%d)", test.bits), func(t *testing.T) {
 			if bytes := bits.ByteCount(test.bits); bytes != test.bytes {
 				t.Errorf("wrong byte count: want=%d got=%d", test.bytes, bytes)
-			}
-		})
-	}
-}
-
-func TestRound(t *testing.T) {
-	for _, test := range []struct {
-		bits  uint
-		round uint
-	}{
-		{bits: 0, round: 0},
-		{bits: 1, round: 8},
-		{bits: 8, round: 8},
-		{bits: 9, round: 16},
-		{bits: 30, round: 32},
-		{bits: 63, round: 64},
-	} {
-		t.Run(fmt.Sprintf("Round(%d)", test.bits), func(t *testing.T) {
-			if round := bits.Round(test.bits); round != test.round {
-				t.Errorf("wrong rounded bit count: want=%d got=%d", test.round, round)
 			}
 		})
 	}
