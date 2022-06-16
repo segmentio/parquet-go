edd371b528ffa7d9b637160d4fbd3b88f82698fa
Achille <achille@segment.com>
2022-06-16 16:39:01 -0700

    Update documentation + add convenience APIs (#242)
    * update documentation + add convenience APIs
    
    * fix example names
    
    * Update README.md
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * Update parquet_go18.go
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * Update reader.go
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * Update writer.go
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>
    
    * rephrase Go version requirements
    
    * declare RowType types in README examples
    
    * use os.Create rather than os.OpenFile
    
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>

---
+++ parquet_go18_test.go
@@ -0,0 +1,50 @@
+//go:build go1.18
+
+package parquet_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/segmentio/parquet-go"
+)
+
+func ExampleReadFile() {
+	type Row struct {
+		ID   int64  `parquet:"id"`
+		Name string `parquet:"name,zstd"`
+	}
+
+	ExampleWriteFile()
+
+	rows, err := parquet.ReadFile[Row]("/tmp/file.parquet")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, row := range rows {
+		fmt.Printf("%d: %q\n", row.ID, row.Name)
+	}
+
+	// Output:
+	// 0: "Bob"
+	// 1: "Alice"
+	// 2: "Franky"
+}
+
+func ExampleWriteFile() {
+	type Row struct {
+		ID   int64  `parquet:"id"`
+		Name string `parquet:"name,zstd"`
+	}
+
+	if err := parquet.WriteFile("/tmp/file.parquet", []Row{
+		{ID: 0, Name: "Bob"},
+		{ID: 1, Name: "Alice"},
+		{ID: 2, Name: "Franky"},
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	// Output:
+}
