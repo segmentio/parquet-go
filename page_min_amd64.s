f88e58831030a7d7f2ded13122b51e963435bc88
Achille Roussel <achille@segment.com>
2022-06-21 17:29:50 +0000

    [#189] do not use MOVBE instruction when compiling for x86

--- page_min_amd64.s
+++ page_min_amd64.s
@@ -560,13 +560,17 @@ loop16:
     //
     // This loop is also take if the CPU has no support for AVX-512.
 loop:
-    MOVBEQQ (AX), R8
-    MOVBEQQ (BX), R9
+    MOVQ (AX), R8
+    MOVQ (BX), R9
+    BSWAPQ R8
+    BSWAPQ R9
     CMPQ R8, R9
     JB less
     JA next
-    MOVBEQQ 8(AX), R8
-    MOVBEQQ 8(BX), R9
+    MOVQ 8(AX), R8
+    MOVQ 8(BX), R9
+    BSWAPQ R8
+    BSWAPQ R9
     CMPQ R8, R9
     JAE next
 less:
