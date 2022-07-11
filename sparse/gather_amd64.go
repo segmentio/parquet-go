aab04c187a91163a3fb7480e15cba2bbea580c53
Achille Roussel <achille@segment.com>
2022-07-11 21:28:43 +0000

    use less assembly and more Go code

--- sparse/gather_amd64.go
+++ sparse/gather_amd64.go
@@ -5,39 +5,67 @@ package sparse
 import "golang.org/x/sys/cpu"
 
 func gatherBits(dst []byte, src Uint8Array) int {
-	if len(dst) >= 16 && cpu.X86.HasAVX2 {
-		return gatherBitsAVX2(dst, src)
+	n := min(len(dst)*8, src.Len())
+	i := 0
+
+	if n >= 8 && cpu.X86.HasAVX2 {
+		i = (n / 8) * 8
+		gatherBitsAVX2(dst, src.Slice(0, i))
 	}
-	return gatherBitsDefault(dst, src)
+
+	for i < n {
+		x := i / 8
+		y := i % 8
+		b := src.Index(i)
+		dst[x] = ((b & 1) << y) | (dst[x] & ^(1 << y))
+		i++
+	}
+
+	return n
 }
 
 func gather32(dst []uint32, src Uint32Array) int {
-	if len(dst) >= 16 && cpu.X86.HasAVX2 {
-		return gather32AVX2(dst, src)
+	n := min(len(dst), src.Len())
+	i := 0
+
+	if n >= 16 && cpu.X86.HasAVX2 {
+		i = (n / 8) * 8
+		gather32AVX2(dst[:i:i], src)
 	}
 
-	n := min(len(dst), src.Len())
-
-	for i := range dst[:n] {
+	for i < n {
 		dst[i] = src.Index(i)
+		i++
 	}
 
 	return n
 }
 
 func gather64(dst []uint64, src Uint64Array) int {
-	if len(dst) >= 8 && cpu.X86.HasAVX2 {
-		return gather64AVX2(dst, src)
+	n := min(len(dst), src.Len())
+	i := 0
+
+	if n >= 8 && cpu.X86.HasAVX2 {
+		i = (n / 4) * 4
+		gather64AVX2(dst[:i:i], src)
 	}
 
-	n := min(len(dst), src.Len())
-
-	for i := range dst[:n] {
+	for i < n {
 		dst[i] = src.Index(i)
+		i++
 	}
 
 	return n
 }
+
+//go:noescape
+func gatherBitsAVX2(dst []byte, src Uint8Array)
+
+//go:noescape
+func gather32AVX2(dst []uint32, src Uint32Array)
+
+//go:noescape
+func gather64AVX2(dst []uint64, src Uint64Array)
 
 //go:noescape
 func gather128(dst [][16]byte, src Uint128Array) int
