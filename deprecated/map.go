5ea0cecbe16b233cbc561198ffc22f71f0941ad3
parsnips <michael@twisp.com>
2022-12-02 16:18:47 -0800

    add some commentary on why this exists.

--- deprecated/map.go
+++ deprecated/map.go
@@ -2,10 +2,15 @@ package deprecated
 
 import "reflect"
 
+// MapGroup is an implementation of the deprecated `repeated group map`
+// syntax that older parquet writers emit.
 type MapGroup[K comparable, V any] map[K]V
 
 func (m MapGroup[K, V]) IsMapGroup() {}
 
+// This interface allows us to use reflection to determine if our
+// map is a MapGroup. Unfortuntely there's no easy way to use
+// reflect.TypeOf on a generic map.
 type isMapGroup interface {
 	IsMapGroup()
 }
