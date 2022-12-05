44dcc46a421bf09f564cc7416ac4c3f5c06cedf1
Achille <achille@segment.com>
2022-12-05 09:23:28 -0800

    add parquet.MultiRowWriter (#424)
    * add parquet.MultiRowWriter
    
    * return io.ErrShortWrite if a writer does not write all the rows

--- row.go
+++ row.go
@@ -138,6 +138,37 @@ type RowWriterWithSchema interface {
 	Schema() *Schema
 }
 
+// MultiRowWriter constructs a RowWriter which dispatches writes to all the
+// writers passed as arguments.
+//
+// When writing rows, if any of the writers returns an error, the operation is
+// aborted and the error returned. If one of the writers did not error, but did
+// not write all the rows, the operation is aborted and io.ErrShortWrite is
+// returned.
+//
+// Rows are written sequentially to each writer in the order they are given to
+// this function.
+func MultiRowWriter(writers ...RowWriter) RowWriter {
+	m := &multiRowWriter{writers: make([]RowWriter, len(writers))}
+	copy(m.writers, writers)
+	return m
+}
+
+type multiRowWriter struct{ writers []RowWriter }
+
+func (m *multiRowWriter) WriteRows(rows []Row) (int, error) {
+	for _, w := range m.writers {
+		n, err := w.WriteRows(rows)
+		if err != nil {
+			return n, err
+		}
+		if n != len(rows) {
+			return n, io.ErrShortWrite
+		}
+	}
+	return len(rows), nil
+}
+
 type forwardRowSeeker struct {
 	rows  RowReader
 	seek  int64
