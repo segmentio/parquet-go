6fdc87840f87431c3c1fdb57631fab5f9bd36b83
parsnips <michael@twisp.com>
2022-12-05 12:05:05 -0800

    hide `isMapGroup`

--- deprecated/map.go
+++ deprecated/map.go
@@ -6,13 +6,13 @@ import "reflect"
 // syntax that older parquet writers emit.
 type MapGroup[K comparable, V any] map[K]V
 
-func (m MapGroup[K, V]) IsMapGroup() {}
+func (m MapGroup[K, V]) isMapGroup() {}
 
 // This interface allows us to use reflection to determine if our
 // map is a MapGroup. Unfortuntely there's no easy way to use
 // reflect.TypeOf on a generic map.
 type isMapGroup interface {
-	IsMapGroup()
+	isMapGroup()
 }
 
 var mapGroupI = reflect.TypeOf((*isMapGroup)(nil)).Elem()
