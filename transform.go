93081c998344548319040768593bcc607f73add6
Achille <achille@segment.com>
2022-12-12 16:34:01 -0800

    Update transform.go
    Co-authored-by: Kevin Burke <kevin.burke@segment.com>

--- transform.go
+++ transform.go
@@ -4,7 +4,7 @@ package parquet
 // to each row rad from reader.
 //
 // The transformation function appends the transformed src row to dst, returning
-// dst and any error that occured during the transformation. If dst is returned
+// dst and any error that occurred during the transformation. If dst is returned
 // unchanged, the row is skipped.
 func TransformRowReader(reader RowReader, transform func(dst, src Row) (Row, error)) RowReader {
 	return &transformRowReader{reader: reader, transform: transform}
