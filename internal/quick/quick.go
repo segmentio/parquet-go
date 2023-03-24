4cd4139e07f9eaabca6d685fe1fa8627b3828263
Bartley Gillan <bartley.gillan@gmail.com>
2023-03-24 16:06:04 -0500

    less verbose

--- internal/quick/quick.go
+++ internal/quick/quick.go
@@ -60,8 +60,8 @@ func (c *Config) Check(f interface{}) error {
 			in := makeArray(n)
 			ok := v.Call([]reflect.Value{in})
 			if !ok[0].Bool() {
-				return fmt.Errorf("test #%d: failed on input of size %d: %#v\n", i+1, n, in.Interface())
-				// return fmt.Errorf("test #%d: failed on input of size %d\n", i+1, n)
+				// return fmt.Errorf("test #%d: failed on input of size %d: %#v\n", i+1, n, in.Interface())
+				return fmt.Errorf("test #%d: failed on input of size %d\n", i+1, n)
 			}
 		}
 	}
